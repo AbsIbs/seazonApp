352b6d398076d4f1fccdc90d4f6f503b85677dbc
Abass Ibrahim <abassdibrahim@gmail.com>
2023-01-08 13:26:18 +0000

    uninstalled AWS/amplify using Amplify delete. Also removed unused assets (images) and unused components

--- seazonApp/src/components/cookingOften.js
+++ seazonApp/src/components/cookingOften.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import { View, StyleSheet, Image, Text, TouchableOpacity } from "react-native";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
 
 const CookingOften = (props) => {
 
